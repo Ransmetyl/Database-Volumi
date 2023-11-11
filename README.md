3f2547858e7113857cf6228fb9ca080bd7e1cc05
Michelangelo Pizzolato <miki124.bello@gmail.com>
2023-11-11 12:56:25 +0100

    Finito Tutto GODO

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Database-Volumi
 Database con inserimento di libri.
+
+Comandi da fare all'interno del dbms:
+
+    Crea database:
+    >create database Esercizio;
+
+    Entra nel database:
+    >use Esercizio;
+
+    Crea tabella:
+
+    >create table volumi(
+
+        isbn int primary key,
+        titolo varchar(50) not null,
+        autore varchar(50) not null,
+        data_prestito date,
+        prestato char(1)
+    
+    );
+
+    Inserimento:
+    >insert into volumi values(1234,"l'informatica mi piace","Mongelli","2023-11-08","N");
+
+    Per la data:
+    >select curdate();  solo la data
+    >select now(); data + orario
+    >select curtime(); solo orario;
+
+    Stampa dei volumi:
+    >select * from volumi;
--- index.php
+++ index.php
@@ -1,65 +1,11 @@
-<!--
-
-    Comandi da fare all'interno del dbms:
-
-        Crea database:
-        >create database Esercizio;
-
-        Entra nel database:
-        >use Esercizio;
-
-        Crea tabella:
-
-        >create table volumi(
-
-            isbn int primary key,
-            titolo varchar(50) not null,
-            autore varchar(50) not null,
-            data_prestito date,
-            prestato char(1);   
-        
-        );
-
-        Inserimento:
-        >insert into volumi values(1234,"l'informatica mi piace","Mongelli","2023-11-08","N");
-
-        Per la data:
-        >select curdate();  solo la data
-        >select now(); data + orario
-        >select curtime(); solo orario;
-
-        Stampa dei volumi:
-        >select * from volumi;
-        
- -->
-
-<style>
-
-    body{
-        font-family: arial;
-        text-align: center;
-    }
-
-    input[type=submit]{
-        padding: 10px;
-        font-size: 15px;
-    }
-
-    .container-elementi{
-        width: 20%;
-        padding-top: 10px;
-        border: 3px solid green;
-        margin: 0 auto;
-    }
-    
-</style>
-
  <!DOCTYPE html>
  <html lang="it">
  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inserimento Libri</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
  </head>
  <body>
     <h1>INSERIMENTO LIBRI</h1>
@@ -79,6 +25,13 @@
             <input type="text" name="autore" id="autore"></input>
             <br />
             <br />
+            <label for="stato">Stato: </label>
+            <select name="stato" id="stato">
+                <option value="P">PRESTATO</option>
+                <option value="N">NON PRESTATO</option>
+            </select>
+            <br />
+            <br />
         </div>
         <br />
         <input type="submit" value="INSERISCI LIBRO" name="inserisci"></input>
@@ -89,37 +42,42 @@
         
         require("funzioni_connessione.inc");
 
-        
+        if(isset($_POST["inserisci"])){
 
-        if(isset($_POST["inserisici"])){
-
-            if(isset($_POST["isbn"]) && !isEmpty($_POST["isbn"])){
-                if(isset($_POST["titolo"]) && !isEmpty($_POST["titolo"])){
-                    if(isset($_POST["autore"]) && !isEmpty($_POST["autore"])){
+            if(isset($_POST["isbn"]) && !empty($_POST["isbn"])){
+                if(isset($_POST["titolo"]) && !empty($_POST["titolo"])){
+                    if(isset($_POST["autore"]) && !empty($_POST["autore"])){
                     
                         $isbn = $_POST["isbn"];
                         $titolo = $_POST["titolo"];
                         $autore = $_POST["autore"];
+                        $stato = $_POST["stato"];
+                        $connessione = connetti('localhost','root','','Esercizio');
+                        $queryEseguita = inserisciVolume($connessione,$isbn,$titolo,$autore,$stato);
 
-                        $connessione = connetti('localhost','root','','Volumi');
-                        $queryEseguita = eseguiQuery($connessione,)
-
-                    }
-                }
-            }
+                        if($queryEseguita)
+                            echo "<br /><div class=works>Query eseguita con successo.</div>";
+                    }else
+                        echo "<br /><div class=error>Errore: Inserire un autore.</div>";
+                }else
+                    echo "<br /><div class=error>Errore: Inserire un titolo.</div>";
+            }else
+                echo "<br /><div class=error>Errore: Inserire un ISBN.</div>";
         }
 
         if(isset($_POST["stampa"])){
             
-            $connessione = connetti('localhost','root','','Volumi');
-            $queryEseguita = eseguiQuery($connessione,'use Volumi;');
-            $queryEseguita = eseguiQuery($connessione,'select * from Volumi');
-            echo $queryEseguita;
-            $connessione->chiudiConnessione();
+            $connessione = connetti('localhost','root','','Esercizio');
+            $queryEseguita = eseguiQuery($connessione,'use Esercizio;');
+            $queryEseguita = eseguiQuery($connessione,'select * from volumi where prestato="'.$_POST["stato"].'"');
+            echo generaTab($queryEseguita);
+            chiudiConnessione($connessione);
         }
-    
+   
+
     ?>
 
-
+    <div class="copyright">© Pizzolato Michelangelo 5AII</div>
+    <div class="nota">Database utilizzato: Esercizio</br>Nome della tabella: volumi</div>
  </body>
  </html>
